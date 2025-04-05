00bfccead38244e7428f285195ad2312642ee5b4
Sergho <sergche04@gmail.com>
2025-04-05 23:47:24 +0300

    feat: add config tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,14 @@
-// Copyright 2022 GHA Test Team
+// Copyright 2020 GHA Test Team
 
 #include <gtest/gtest.h>
+
 #include "Automata.h"
+
+TEST(config, exists) {
+  Automata automata("products.json");
+  EXPECT_EQ(0, automata.getState());
+}
+
+TEST(config, notFound) {
+  EXPECT_THROW({ Automata automata("notFound.json"); }, std::invalid_argument);
+}
