d3b19829a70a82a3e0e49aed7bb5a22f53985057
Sergho <sergche04@gmail.com>
2025-04-05 23:54:30 +0300

    feat: add state tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,10 +5,86 @@
 #include "Automata.h"
 
 TEST(config, exists) {
-  Automata automata("products.json");
-  EXPECT_EQ(0, automata.getState());
+  EXPECT_NO_THROW({ Automata automata("products.json"); });
 }
 
 TEST(config, notFound) {
   EXPECT_THROW({ Automata automata("notFound.json"); }, std::invalid_argument);
+}
+
+TEST(state, initial) {
+  Automata automata("products.json");
+  EXPECT_EQ(0, automata.getState());
+}
+
+TEST(state, wait) {
+  Automata automata("products.json");
+  automata.on();
+  EXPECT_EQ(1, automata.getState());
+}
+
+TEST(state, off) {
+  Automata automata("products.json");
+  automata.on();
+  automata.off();
+  EXPECT_EQ(0, automata.getState());
+}
+
+TEST(state, accept) {
+  Automata automata("products.json");
+  automata.on();
+  automata.coin(100);
+  EXPECT_EQ(2, automata.getState());
+}
+
+TEST(state, doubleAccept) {
+  Automata automata("products.json");
+  automata.on();
+  automata.coin(100);
+  automata.coin(100);
+  EXPECT_EQ(2, automata.getState());
+}
+
+TEST(state, acceptCancel) {
+  Automata automata("products.json");
+  automata.on();
+  automata.coin(100);
+  automata.cancel();
+  EXPECT_EQ(1, automata.getState());
+}
+
+TEST(state, check) {
+  Automata automata("products.json");
+  automata.on();
+  automata.coin(100);
+  automata.choice(1);
+  EXPECT_EQ(3, automata.getState());
+}
+
+TEST(state, checkCancel) {
+  Automata automata("products.json");
+  automata.on();
+  automata.coin(100);
+  automata.choice(1);
+  automata.cancel();
+  EXPECT_EQ(1, automata.getState());
+}
+
+TEST(state, cook) {
+  Automata automata("products.json");
+  automata.on();
+  automata.coin(1000);
+  automata.choice(1);
+  automata.cook();
+  EXPECT_EQ(4, automata.getState());
+}
+
+TEST(state, finish) {
+  Automata automata("products.json");
+  automata.on();
+  automata.coin(1000);
+  automata.choice(1);
+  automata.cook();
+  automata.finish();
+  EXPECT_EQ(1, automata.getState());
 }
