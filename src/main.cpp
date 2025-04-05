c5413aabed8dfd3c53235cde34e06a1fd088c830
Sergho <sergche04@gmail.com>
2025-04-05 23:19:00 +0300

    feat: create main case

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,69 @@
 #include "States.h"
 
 using namespace std;
+void logInfo(Automata automata) {
+  cout << "Текущий баланс: " << automata.getCash() << endl;
+  string selected = automata.getSelected();
+  if (selected == "") {
+    cout << "Сейчас ничего не выбрано" << endl;
+  } else {
+    cout << "Сейчас выбрано: " << automata.getSelected() << endl;
+    if (!automata.check()) {
+      cout << "Можем себе позволить" << endl;
+    } else {
+      cout << "Слишком дорого" << endl;
+    }
+  }
+  cout << "Состояние: " + automata.getStateDesc() << endl << endl;
+}
 int main() {
   Automata automata("products.json");
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
+  cout << "Создан кофейный автомат" << endl;
+  logInfo(automata);
+
+  cout << "Включение..." << endl;
   automata.on();
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
-  cout << automata.getMenu();
+  logInfo(automata);
+
+  cout << automata.getMenu() << endl;
+
+  cout << "Вносим 70 рублей..." << endl;
   automata.coin(70);
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
+  logInfo(automata);
+
+  cout << "Вносим 50 рублей..." << endl;
   automata.coin(50);
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
+  logInfo(automata);
+
+  cout << "Выбираем Раф-кофе..." << endl;
   automata.choice(9);
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
-  cout << automata.getSelected() << endl;
-  cout << automata.check() << endl;
+  logInfo(automata);
+
+  cout << "Передумал. Отмена..." << endl;
   automata.cancel();
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
-  automata.coin(60);
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
+  logInfo(automata);
+
+  cout << "Вносим 40 рублей..." << endl;
+  automata.coin(40);
+  logInfo(automata);
+
+  cout << "Выбираем Эспрессо..." << endl;
   automata.choice(1);
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
-  cout << automata.getSelected() << endl;
-  cout << automata.check() << endl;
+  logInfo(automata);
+
+  cout << "Отмена..." << endl;
+  automata.cancel();
+  logInfo(automata);
+
+  cout << "Вносим 50 рублей..." << endl;
+  automata.coin(50);
+  logInfo(automata);
+
+  cout << "Начинаем готовить..." << endl;
   automata.cook();
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
+  logInfo(automata);
+
+  cout << "Кофе готов!" << endl;
   automata.finish();
-  cout << automata.getStateDesc() << " " << automata.getCash() << endl;
+  logInfo(automata);
 }
