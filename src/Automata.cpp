37a16eedfeb4eaea7f6e9ef1584f1020b591a142
Sergho <sergche04@gmail.com>
2025-04-05 18:57:43 +0300

    feat: implement `coin` method

--- src/Automata.cpp
+++ src/Automata.cpp
@@ -2,3 +2,7 @@
 
 void Automata::on() { this->state = State::WAIT; }
 void Automata::off() { this->state = State::OFF; }
+int Automata::coin(int amount) {
+  this->cash += amount;
+  return this->cash;
+}
